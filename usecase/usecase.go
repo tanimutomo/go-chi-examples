c720ef706d4f4b107039b3b4c4bd9fe29af78334
tanimutomo <tanimutomo@gmail.com>
2021-02-06 17:53:50 +0900

    add: usecase/RandomSleepInDB

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -1,13 +1,26 @@
 package usecase
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/tanimutomo/go-chi-examples/repository"
 )
 
+const prefix = "    [Usecase] "
+
+type Input struct {
+	Name string
+}
+
+type Output struct {
+	Greeting string
+}
+
 func RandomSleep(ipt Input) Output {
-	fmt.Println("    [Usecase] start")
+	fmt.Println(prefix, "start")
 	begin := time.Now()
 
 	// Processing will take 1-5 seconds.
@@ -17,18 +30,28 @@ func RandomSleep(ipt Input) Output {
 	time.Sleep(processTime)
 
 	end := time.Now()
-	fmt.Println("    [Usecase] end")
-	fmt.Println("    [Usecase] duration: ", end.Sub(begin).Seconds())
+	fmt.Println(prefix, "end")
+	fmt.Println(prefix, "duration: ", end.Sub(begin).Seconds())
 
 	return Output{
 		Greeting: fmt.Sprintf("Hi, %s", ipt.Name),
 	}
 }
 
-type Input struct {
-	Name string
-}
+func RandomSleepInDB(ctx context.Context, ipt Input) (Output, error) {
+	fmt.Println(prefix, "start")
+	begin := time.Now()
 
-type Output struct {
-	Greeting string
+	err := repository.DoSomethingWithRandomSleep(ctx)
+	if err != nil {
+		return Output{}, err
+	}
+
+	end := time.Now()
+	fmt.Println(prefix, "end")
+	fmt.Println(prefix, "duration: ", end.Sub(begin).Seconds())
+
+	return Output{
+		Greeting: fmt.Sprintf("Hi, %s", ipt.Name),
+	}, nil
 }
